b8090c46ae43dee183aae66be7b64f8052988097
Lynzie Johnson <lynzie.jjohnson@gmail.com>
2017-10-16 11:36:42 -0600

    Added new and edit buttons for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20171012221226) do
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
-    t.string "titl"
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
