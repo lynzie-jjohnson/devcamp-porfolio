8e7b38f973c3680270af4dfa8c6fa6b53d5f2450
Lynzie Johnson <lynzie.jjohnson@gmail.com>
2017-10-24 13:50:46 -0600

    Implemented bootstrap gem and styles

--- Gemfile
+++ Gemfile
@@ -54,3 +54,7 @@ gem 'friendly_id', '~> 5.1.0'
 
 gem 'devise', '~> 4.3'
 
+gem 'bootstrap', '~> 4.0.0.beta2.1'
+
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs 
 //= require rails-ujs
+//= require jquery3
+//= require popper
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
+
