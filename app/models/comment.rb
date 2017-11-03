c552f0214f59fdb25f8885b96232a2104ae4bb1c
Lynzie Johnson <lynzie.jjohnson@gmail.com>
2017-11-03 11:35:12 -0600

    fixed sytax error in comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
 
-  validates :content, presence :true, length: {minimun: 5, maximum: 1000}
+  validates :content, presence: true, length: { minimun: 5, maximum: 1000 }
 end
