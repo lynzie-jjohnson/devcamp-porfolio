bf01a1490054fb9892b4601301b12f3688412a8a
Lynzie Johnson <lynzie.jjohnson@gmail.com>
2017-12-05 11:46:12 -0700

    Reordered application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,16 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery
 //= require jquery_ujs
-//= require jquery-ui 
-//= require popper
-//= require rails-ujs
-
+//= require jquery-ui
 //= require html.sortable
-
 //= require cocoon
 //= require gritter
+//= require lib/typed
+//= require typed-implementation
 //= require cable
 //= require turbolinks
 //= require_tree .
